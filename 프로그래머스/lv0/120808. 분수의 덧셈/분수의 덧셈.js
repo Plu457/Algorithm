747dba2d5820815d9acb38f3aa8f884988666751
Pulan <ekgus5095@gmail.com>
2022-10-19 20:33:59 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,13 +1,13 @@
+function CalculateGCD(denominator, numerator) {
+  return denominator % numerator
+    ? CalculateGCD(numerator, denominator % numerator)
+    : numerator;
+}
+
 function solution(denum1, num1, denum2, num2) {
   const denominator = num1 * denum2 + num2 * denum1;
   const numerator = num1 * num2;
+  const GCD = CalculateGCD(denominator, numerator);
 
-  let LCM = null;
-  for (let i = 1; i <= denominator; i++) {
-    if (denominator % i === 0 && numerator % i === 0) {
-      LCM = i;
-    }
-  }
-    
-  return [denominator / LCM, numerator / LCM];
+  return [denominator / GCD, numerator / GCD];
 }
