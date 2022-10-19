8d57daffe444c9841761946aa146f6cf247ae467
Pulan <ekgus5095@gmail.com>
2022-10-19 20:26:28 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,13 +1,13 @@
 function solution(denum1, num1, denum2, num2) {
-  const numerator = num1 * denum2 + num2 * denum1;
-  const denominator = num1 * num2;
+  const denominator = num1 * denum2 + num2 * denum1;
+  const numerator = num1 * num2;
 
   let LCM = null;
-  for (let i = 1; i <= numerator; i++) {
-    if (numerator % i === 0 && denominator % i === 0) {
+  for (let i = 1; i <= denominator; i++) {
+    if (denominator % i === 0 && numerator % i === 0) {
       LCM = i;
     }
   }
     
-  return [numerator / LCM, denominator / LCM];
+  return [denominator / LCM, numerator / LCM];
 }
