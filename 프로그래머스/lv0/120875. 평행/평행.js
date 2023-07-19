b89f7d935c7b9707c0fc54649eab314b7ba46bbb
Dahyun Kim <ekgus5095@gmail.com>
2023-07-19 12:42:32 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.js
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -1,6 +1,14 @@
 const calculateSlope = (dot1, dot2) => (dot2[1] - dot1[1]) / (dot2[0] - dot1[0]);
 
 function solution(dots) {
-    return calculateSlope(dots[0], dots[1]) === calculateSlope(dots[2], dots[3]) || 
-           calculateSlope(dots[0], dots[2]) === calculateSlope(dots[1], dots[3]) ? 1 : 0;
+    const slope1 = calculateSlope(dots[0], dots[1]);
+    const slope2 = calculateSlope(dots[2], dots[3]);
+    const slope3 = calculateSlope(dots[0], dots[2]);
+    const slope4 = calculateSlope(dots[1], dots[3]);
+
+    if (slope1 === slope2 || slope3 === slope4) {
+        return 1;
+    }
+
+    return 0;
 }
