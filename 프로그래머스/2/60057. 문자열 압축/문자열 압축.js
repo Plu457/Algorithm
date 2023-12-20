88e7f0aa7a4396662867ef4479299c8288198f0c
Dahyun Kim <ekgus5095@gmail.com>
2023-12-20 20:55:56 +0900

    [level 2] Title: 문자열 압축, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -1,30 +1,34 @@
-const sliceString = (s, size) => {
-    return Array.from({ length: Math.ceil(s.length / size) }, (_, i) => {
-        return s.substring(i * size, (i + 1) * size);
+// 주어진 크기로 문자열을 잘라 배열로 반환
+const sliceString = (str, size) => {
+    return Array.from({ length: Math.ceil(str.length / size) }, (_, i) => {
+        return str.substring(i * size, (i + 1) * size);
     });
 };
 
+// 문자열 조각 배열을 압축하여 하나의 문자열로 만들기
 const compressString = (chunks) => {
-    return chunks.reduce((compressed, chunk, i, arr) => {
-        if (i === 0 || chunk !== arr[i - 1]) {
-            compressed.push({ chunk, count: 1 });
+    return chunks.reduce((compressed, currentChunk, i, chunksArr) => {
+        if (i === 0 || currentChunk !== chunksArr[i - 1]) {
+            // 현재 조각이 이전 조각과 다르면 새로운 조각으로 시작
+            compressed.push({ chunk: currentChunk, count: 1 });
         } else {
+            // 현재 조각이 이전 조각과 같으면 카운트를 증가
             compressed[compressed.length - 1].count++;
         }
         return compressed;
-    }, []).map(({ count, chunk }) => {
-        return count > 1 ? count + chunk : chunk;
-    }).join('');
+    }, []).map(({ count, chunk }) => count > 1 ? count + chunk : chunk).join('');
+    // 각 조각을 '카운트 + 조각' 형태로 문자열로 변환합니다.
 };
 
-const solution = (s) => {
-    if (s.length === 1) return 1;
+// 주어진 문자열을 가장 효율적으로 압축할 때의 길이를 반환하는 함수
+// 문자열을 다양한 크기로 나누어 압축한 후, 가장 짧은 압축 길이를 찾기
+const solution = (str) => {
+    if (str.length === 1) return 1;
 
-    return Array.from({ length: Math.floor(s.length / 2) }, (_, i) => i + 1)
-        .map((size) => {
-            return compressString(sliceString(s, size)).length;
-        })
-        .reduce((min, length) => {
-            return Math.min(min, length);
-        }, s.length);
+    // 가능한 모든 분할 크기에 대해 압축한 문자열의 길이를 계산
+    const compressedLengths = Array.from({ length: Math.floor(str.length / 2) }, (_, i) => i + 1)
+        .map(size => compressString(sliceString(str, size)).length);
+
+    // 계산된 길이 중 가장 작은 값을 찾기
+    return compressedLengths.reduce((minLength, currentLength) => Math.min(minLength, currentLength), str.length);
 };
