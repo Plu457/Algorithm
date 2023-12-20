58c0ddabe6ac25bd6c1b4c69619bbe11b8a3fe05
Dahyun Kim <ekgus5095@gmail.com>
2023-12-20 20:49:25 +0900

    [level 2] Title: 문자열 압축, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -1,27 +1,30 @@
 const sliceString = (s, size) => {
-  return Array.from({ length: Math.ceil(s.length / size) }, (_, i) =>
-    s.substring(i * size, (i + 1) * size),
-  );
+    return Array.from({ length: Math.ceil(s.length / size) }, (_, i) => {
+        return s.substring(i * size, (i + 1) * size);
+    });
 };
 
-const compressString = segments => {
-  const compressed = segments.reduce((acc, segment, i, arr) => {
-    if (i === 0 || segment !== arr[i - 1]) {
-      acc.push({ segment, count: 1 });
-    } else {
-      acc[acc.length - 1].count++;
-    }
-    return acc;
-  }, []);
-
-  return compressed.map(({ count, segment }) => (count > 1 ? count + segment : segment)).join('');
+const compressString = (chunks) => {
+    return chunks.reduce((compressed, chunk, i, arr) => {
+        if (i === 0 || chunk !== arr[i - 1]) {
+            compressed.push({ chunk, count: 1 });
+        } else {
+            compressed[compressed.length - 1].count++;
+        }
+        return compressed;
+    }, []).map(({ count, chunk }) => {
+        return count > 1 ? count + chunk : chunk;
+    }).join('');
 };
 
-const solution = s => {
-  if (s.length === 1) return 1;
+const solution = (s) => {
+    if (s.length === 1) return 1;
 
-  const range = Array.from({ length: Math.floor(s.length / 2) }, (_, i) => i + 1);
-  const compressedLengths = range.map(size => compressString(sliceString(s, size)).length);
-
-  return Math.min(...compressedLengths, s.length);
+    return Array.from({ length: Math.floor(s.length / 2) }, (_, i) => i + 1)
+        .map((size) => {
+            return compressString(sliceString(s, size)).length;
+        })
+        .reduce((min, length) => {
+            return Math.min(min, length);
+        }, s.length);
 };
